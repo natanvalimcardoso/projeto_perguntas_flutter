02bbebc56ec08ea8d5baaac8f5a48231a35c7785
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2021-10-28 15:03:44 -0300

    novo

--- lib/resposta.dart
+++ lib/resposta.dart
@@ -2,19 +2,26 @@ import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
   final String texto;
+  final void Function() quandoSelecionado;
 
-  Resposta(this.texto, void Function() responder);
+  Resposta(this.texto, this.quandoSelecionado);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-              primary: Colors.blue,
-              textStyle: TextStyle(color: Colors.amberAccent)),
-          onPressed: () {},
-          child: Text(texto)),
+        style: ElevatedButton.styleFrom(
+          primary: Colors.blue,
+        ),
+        child: Text(
+          texto,
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        onPressed: quandoSelecionado,
+      ),
     );
   }
 }
